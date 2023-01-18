3a938bd0318588466a3fb595be6b42b013f40f7d
isabelcoboruizazuaga <59738137+isabelcoboruizazuaga@users.noreply.github.com>
2023-01-18 13:41:10 +0100

    base de datos funcionando

---
+++ database/migrations/2023_01_17_133223_create_asignaturas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asignaturas', function (Blueprint $table) {
+            $table->string("codAs")->primary();
+            $table->unsignedBigInteger("user_id");
+            $table->string('nombreAs');
+            $table->string('nombreCortoAs');
+            $table->integer('profesorAs');
+            $table->string('colorAs');
+            $table->timestamps();
+
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asignaturas');
+    }
+};
